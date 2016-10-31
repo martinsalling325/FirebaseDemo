34b6491902b67a95d65a0e3a36623a87ebaf6520
MS <martinsalling325@hotmail.com>
2016-10-31 13:23:59 +0100

    Android Charts added

--- app/src/main/java/com/example/ms/firebase/MainActivity.java
+++ app/src/main/java/com/example/ms/firebase/MainActivity.java
@@ -14,6 +14,12 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+
+import im.dacer.androidcharts.LineView;
+import im.dacer.androidcharts.PieHelper;
+import im.dacer.androidcharts.PieView;
+
 public class MainActivity extends AppCompatActivity {
 
     private DatabaseReference myDatabase;
@@ -37,6 +43,56 @@ public class MainActivity extends AppCompatActivity {
         editTextName = (EditText) findViewById(R.id.editTextCompanyName);
         editTextExchange = (EditText) findViewById(R.id.editTextExchange);
         editTextSymbol = (EditText) findViewById(R.id.editTextSymbol);
+
+        ArrayList<String> strList = new ArrayList<>();
+        strList.add("Dette");
+        strList.add("Er");
+        strList.add("En");
+        strList.add("Test");
+        strList.add("bla");
+
+        ArrayList<ArrayList<Integer>> dataLists = new ArrayList<>();
+
+        ArrayList<Integer> integers = new ArrayList<>();
+        integers.add(200);
+        integers.add(500);
+        integers.add(1000);
+        integers.add(720);
+        integers.add(400);
+        dataLists.add(integers);
+
+        ArrayList<Integer> integers2 = new ArrayList<>();
+        integers2.add(1200);
+        integers2.add(1500);
+        integers2.add(1300);
+        integers2.add(820);
+        integers2.add(600);
+        dataLists.add(integers2);
+
+        LineView lineView = (LineView)findViewById(R.id.line_view);
+        lineView.setDrawDotLine(false); //optional
+        lineView.setShowPopup(LineView.SHOW_POPUPS_MAXMIN_ONLY); //optional
+        lineView.setBottomTextList(strList);
+        lineView.setDataList(dataLists);
+
+        PieView pieView = (PieView)findViewById(R.id.pie_view);
+        ArrayList<PieHelper> pieHelperArrayList = new ArrayList<>();
+
+        PieHelper p1 = new PieHelper(12);
+        PieHelper p2 = new PieHelper(40);
+        PieHelper p3 = new PieHelper(8);
+        PieHelper p4 = new PieHelper(30);
+        PieHelper p5 = new PieHelper(10);
+        pieHelperArrayList.add(p1);
+        pieHelperArrayList.add(p2);
+        pieHelperArrayList.add(p3);
+        pieHelperArrayList.add(p4);
+        pieHelperArrayList.add(p5);
+
+        pieView.setDate(pieHelperArrayList);
+        pieView.selectedPie(2); //optional
+        //pieView.setOnPieClickListener(listener) //optional
+        pieView.showPercentLabel(true); //optional
     }
 
     public void buttonWriteClicked(View view)
@@ -67,7 +123,5 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-
-
     }
 }
